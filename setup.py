8e61d75cbad405fa9a2a87a8e9c551124c1b6a3a
smartwa <smartwacaleb@gmail.com>
2025-01-17 17:05:29 +0300

    chore: Bump version number from 0.2.0 to 0.2.1

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name="yt-dlp-bonus",
-    version="0.2.0",
+    version="0.2.1",
     license="The Unlicense",
     author="Smartwa",
     maintainer="Smartwa",
