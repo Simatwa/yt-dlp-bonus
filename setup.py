401a0fbbe92087c0f84c89337f41cf903e1992b5
Simatwa <smartwacaleb@gmail.com>
2025-02-02 00:43:46 +0300

    feat: Add typer dependency to requirements.txt and setup.py

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ setup(
     entry_points={
         "console_scripts": ["yt-dlpb = yt_dlp_bonus.cli:app"],
     },
-    install_requires=["yt-dlp>=2024.12.13", "pydantic==2.9.2"],
+    install_requires=["yt-dlp>=2024.12.13", "pydantic==2.9.2", "typer==0.15.1"],
     python_requires=">=3.10",
     keywords=[
         "yt-dlp",
